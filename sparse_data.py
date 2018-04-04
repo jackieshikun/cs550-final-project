070e31ce5018360035383fc8bca3e47218dc5bef
chouxi <qizheng1993hit@gmail.com>
2018-04-03 20:40:05 -0400

    Separate train and test set after reading json file

--- sparse_data.py
+++ sparse_data.py
@@ -1,5 +1,7 @@
 import json
 from review_enum import review_enum
+import random
+import numpy as np
 class sparse_data:
     __row2user_dict = {}
     __user2row_dict = {}
@@ -7,13 +9,17 @@ class sparse_data:
     __item2col_dict = {}
     #(row,col), [rate, [helpful, unhelpful], review_text]
     __data = {}
-    __row_ind = []
-    __col_ind = []
+    __row_train_ind = []
+    __col_train_ind = []
+    __row_test_ind = []
+    __col_test_ind = []
     def __init__(self, filename):
         with open(filename, "r") as f:
             lines = f.readlines()
         row_count = 0
         col_count = 0
+        row_ind = []
+        col_ind = []
         for line in lines:
             record = json.loads(line)
             userId = record['reviewerID']
@@ -29,8 +35,35 @@ class sparse_data:
             row_idx_tmp = self.__user2row_dict[userId]
             col_idx_tmp = self.__item2col_dict[itemId]
             self.__data[(row_idx_tmp, col_idx_tmp)] = [float(record['overall']), record['helpful'], record['reviewText']]
-            self.__row_ind.append(row_idx_tmp)
-            self.__col_ind.append(col_idx_tmp)
+            row_ind.append(row_idx_tmp)
+            col_ind.append(col_idx_tmp)
+        sorted_idx = np.argsort(row_ind)
+        row_ind = np.array(row_ind)
+        col_ind = np.array(col_ind)
+        row_ind = row_ind[sorted_idx]
+        col_ind = col_ind[sorted_idx]
+        self.__seperate_train_test(row_ind, col_ind)
+
+    # default hold 20% of items purchesed for each user as test data.
+    def __seperate_train_test(self, row_idx, col_idx, prob=0.2):
+        ptr = 0
+        i = 0
+        size = len(row_idx)
+        while i < size:
+            while ptr < size and row_idx[i] == row_idx[ptr]:
+                ptr += 1
+            count = ptr - i
+            if not count == 0:
+                rand_size = int(count * prob)
+                rand_idx = random.sample(range(0, count), rand_size)
+                if not len(rand_idx) == 0:
+                    self.__row_test_ind += list(row_idx[np.add(rand_idx, i)])
+                    self.__col_test_ind += list(col_idx[np.add(rand_idx, i)])
+                train_idx = [item for item in range(count) if item not in rand_idx]
+                if not len(train_idx) == 0:
+                    self.__row_train_ind += list(row_idx[np.add(train_idx, i)])
+                    self.__col_train_ind += list(col_idx[np.add(train_idx, i)])
+            i = ptr
 
     def get_row_size(self):
         return len(self.__row2user_dict)
@@ -75,8 +108,14 @@ class sparse_data:
             return -1
         return self.__user2row_dict[userId]
 
-    def get_row_list(self):
-        return self.__row_ind
+    def get_train_row_list(self):
+        return self.__row_train_ind
 
-    def get_col_list(self):
-        return self.__col_ind
+    def get_train_col_list(self):
+        return self.__col_train_ind
+
+    def get_test_row_list(self):
+        return self.__row_test_ind
+
+    def get_test_col_list(self):
+        return self.__col_test_ind
