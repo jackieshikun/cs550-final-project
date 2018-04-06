eb5379e81586e320ec2ccb4c4dc9d4c44c84f55b
chouxi <qizheng1993hit@gmail.com>
2018-04-05 21:37:39 -0400

    SGD of latent factor (without testing)

--- latent_factor.py
+++ latent_factor.py
@@ -3,7 +3,7 @@ from scipy import sparse
 from sklearn.metrics import pairwise_distances
 import numpy as np
 
-# Return: rating_matrix, overall mean, user bias, item bias
+# Return: val_list, overall mean, user bias, item bias
 def preprocess(train_data):
   row_list = train_data.get_train_row_list()
   col_list = train_data.get_train_col_list()
@@ -32,8 +32,8 @@ def preprocess(train_data):
     rec = col_sum[i]
     item_bias[i] = float(rec[0]) / rec[1]
   overall_mean = np.sum(val_list) / len(val_list)
-  rating_mat = sparse.csr_matrix((val_list, (row_list, col_list)), dtype=float)
-  return rating_mat, overall_mean, user_bias, item_bias
+  #rating_mat = sparse.csr_matrix((val_list, (row_list, col_list)), dtype=float)
+  return val_list, overall_mean, user_bias, item_bias
 
 # Based on the sigma of SVD to decide how many factors to keep in latent factor
 def est_dimension(sparse_matrix, energy=0.8):
@@ -47,19 +47,46 @@ def est_dimension(sparse_matrix, energy=0.8):
       break
   return dimension
 
-#return p,q,b_i,b_u
-def fit(train_data, learning_rate_list, error, iter_num):
+#return b_u, b_i, p, q
+def fit(train_data, learning_rate_list, regulation_rate_list, error, iter_num):
   i = 0
-  csr = gen_rating_mat(train_data)
+  rating_csr, overall_mean, user_bias, item_bias = preprocess(train_data)
   factors = est_dimension(csr)
-  users = train_data.get_row_size()
-  items = train_data.get_col_size()
+  user_size = train_data.get_row_size()
+  item_size = train_data.get_col_size()
 
-  p = np.zeros(users, factors)
-  q = np.zeros(items, factors)
-  b_u = np.zeros(users, np.double)
-  b_i = np.zeros(items, np.double)
+  # mean, std, (shape)
+  p = np.random.normal(0, 0.1, (user_size, factors))
+  q = np.random.normal(0, 0.1, (item_size, factors))
+
+  b_u_lr = learning_rate_list[0]
+  b_i_lr = learning_rate_list[1]
+  p_lr = learning_rate_list[2]
+  q_lr = learning_rate_list[3]
+  b_u_rg = regulation_rate_list[0]
+  b_i_rg = regulation_rate_list[1]
+  p_rg = regulation_rate_list[3]
+  q_rg = regulation_rate_list[4]
+
+  row_list = train_data.get_train_row_list()
+  col_list = train_data.get_train_col_list()
   while i < iter_num:
+    print("Processing iteration {}".format(i))
+    total_err = 0
+    for idx in range(len(val_list)):
+      r = row_list[idx]
+      c = col_list[idx]
+      r = val_list[idx]
+      pr = r[r]
+      qc = q[c]
+      err = r - (overall_mean + b_u[r] + b_i[c] + np.dot(pr, qc.T)
+      total_err += err ** 2
+      b_u[r] += b_u_lr * (err - b_u_rg * b_u[r])
+      b_i[c] += b_i_lr * (err - b_i_rg * b_i[c])
+      p[r] += p_lr * (err * qc + p_rg * pr)
+      q[c] += q_lr * (err * pr + q_rt * qc)
+    if total_err <= error:
+      break
     i += 1
 
 if __name__ == '__main__':
@@ -75,6 +102,6 @@ if __name__ == '__main__':
   #print(data.get_val(0,0, 'rating'))
   #print(data.get_val(0,0, 'reviewText'))
   #print(data.get_entry_size())
-  rating_mat, overall_mean, user_bias, item_bias = gen_rating_mat(data)
+  rating_mat, overall_mean, user_bias, item_bias = preprocess(data)
   #print(pairwise_distances(rating_mat))
   #est_dimension(rating_mat)
