4c985510a6e98af170189aae73f667c64be1fa0f
shikunli <sl1395@scarletmail.rutgers.edu>
2018-04-12 19:16:34 -0400

    fix bugs in sample algorithm

--- surprise_sample.py
+++ surprise_sample.py
@@ -1,8 +1,11 @@
+# In[3]:
+
 # encoding = utf-8
 import json
 from pprint import pprint
 import pandas as pd
 
+from surprise import NormalPredictor
 from surprise import Dataset
 from surprise import Reader
 from surprise import SVD
@@ -13,51 +16,63 @@ from surprise import KNNBasic
 from surprise import KNNBaseline
 from surprise import SVDpp
 from collections import defaultdict
-from surprise.model_selection import GridSearchCV
 from surprise.model_selection import train_test_split
+import sparse_data as sp
+from surprise.model_selection import PredefinedKFold
 
 
-# In[60]:
+# In[7]:
 
 ### preprecess
 
 filename = "test.json"
-
-def preprocess(filename):
-  jsonFile = file(filename, 'r')
-  data = jsonFile.read()
-  lines = data.split('\n')
-  itemIDList = []
-  userIDList = []
-  ratingList = []
-  for line in lines:
-    if len(line) == 0:
-      continue;
-    review = json.loads(line)
-    itemIDList.append(review['asin'])
-    userIDList.append(review['reviewerID'])
-    ratingList.append(float(review['overall']))
-  ratings_dict = {'userID': userIDList,
-                    'itemID': itemIDList,
-                    'rating': ratingList}
-  df = pd.DataFrame(ratings_dict)
-  return df
+trainFile = "train.txt"
+testFile = "test.txt"
 #filename = "reviews_Electronics_5.json"
-df = preprocess(filename)
-reader = Reader(rating_scale=(1, 5))
-data = Dataset.load_from_df(df[['userID', 'itemID', 'rating']], reader)
 
-# split data into trainset and testset
-trainset, testset = train_test_split(data, test_size =.25)
+# extract data from filename and generate trainset file and testset file.
+def preprocess(filename, trainFile, testFile):
+    raw_data = sp.sparse_data(filename)
+    train_output = trainFile
+    train_f = open(train_output,"w")
+    train_row_ind = raw_data.get_train_row_list()
+    train_col_ind = raw_data.get_train_col_list()
+    for i in range(len(train_row_ind)):
+        row = train_row_ind[i]
+        col = train_col_ind[i]
+        user = raw_data.get_userID(row)
+        item = raw_data.get_itemID(col)
+        value = raw_data.get_val(row, col, 'rating')
+        train_f.write(user+"\t"+item+"\t"+str(value)+"\n")
+    train_f.close()
+    
+    test_output = testFile
+    test_f = open(test_output, "w")
+    test_row_ind = raw_data.get_test_row_list()
+    test_col_ind = raw_data.get_test_col_list()
+    for i in range(len(test_row_ind)):
+        row = test_row_ind[i]
+        col = test_col_ind[i]
+        user = raw_data.get_userID(row)
+        item = raw_data.get_itemID(col)
+        value = raw_data.get_val(row, col, 'rating')
+        test_f.write(user+"\t"+item+"\t"+str(value)+"\n")
+    test_f.close()
+   
+preprocess(filename, trainFile, testFile)
+folds_files = [(trainFile, testFile)]
+reader = Reader(line_format='user item rating', sep='\t')
+data = Dataset.load_from_folds(folds_files, reader=reader)
+pkf = PredefinedKFold()
 
 
-# In[61]:
+# In[5]:
 
 ### set Paramter(baseline and similarity)
 
 ### bsl: sgd   als
 bsl_options = {'method': 'sgd',
-               'n_epochs': 50,
+               'n_epochs': 20,
                'learning_rate': 0.05,
                }
 ### sim name: cosine    msd       pearson     pearson_baseline
@@ -67,7 +82,56 @@ sim_options = {'name': 'cosine',
               'user_based':True}
 
 
-# In[62]:
+# In[6]:
+
+### recommendation
+def get_top_n(predictions, n=10, threshold = 3.0):
+    # First map the predictions to each user.
+    top_n = defaultdict(list)
+    for uid, iid, true_r, est, _ in predictions:
+        top_n[uid].append((iid, est))
+
+    # Then sort the predictions for each user and retrieve the k highest ones.
+    for uid, user_ratings in top_n.items():
+        user_ratings.sort(key=lambda x: x[1], reverse=True)
+        top_n[uid] = user_ratings[:n]
+        #top_n[uid] = [rate for rate in user_ratings if rate[1] >= threshold]
+    return top_n
+
+def calculate_percision(top_n, testSet):
+    # get true recommend set
+    trueItemSet = {}
+    for user,item,score in testSet:
+        if user not in trueItemSet:
+            trueItemSet[user] = set()
+        trueItemSet[user].add(item)
+    # get prediction recommend set
+    predictionSet = {}
+    for user, user_ratings in top_n.items():
+        if user not in predictionSet:
+            predictionSet[user] = set()
+        if len(user_ratings) == 0:
+            continue
+        predictionSet[user].add(user_ratings[0])
+    # calculate match rate
+    matchCount = 0.0
+    countDic = {}
+    temp = 0
+    for k, v in predictionSet.iteritems():
+        if k not in trueItemSet:
+            countDic[k] = 0
+            continue
+        temp += 1
+        pSet = trueItemSet[k]
+        count = 0.0
+        for ele in v:
+            if ele in pSet:
+                count+=1
+                matchCount+=1
+        if count > 0:
+            print count
+        countDic[k] = count / len(v)
+    return countDic, matchCount / len(predictionSet)
 
 ### train
 
@@ -78,43 +142,19 @@ sim_options = {'name': 'cosine',
 
 # combine neighboors' value to calculate similarity
 #algo = KNNBasic(bsl_options=bsl_options, sim_options=sim_options) 
-
-algo = SVD()
-
-algo.fit(trainset)
-
-
-# In[63]:
-
-### test
-predictions = algo.test(testset)
-
-### accuracy:   rmse(root mean square error)     mae(mean absolute error)      fcp
-# Compute and print Root Mean Squared Error
-accuracy.rmse(predictions, verbose=True)
-
-
-# In[80]:
-
-### recommendation
-def get_top_n(predictions, n=10, threshold = 4.0):
-  # First map the predictions to each user.
-  top_n = defaultdict(list)
-  for uid, iid, true_r, est, _ in predictions:
-    top_n[uid].append((iid, est))
-  # Then sort the predictions for each user and retrieve the k highest ones.
-  for uid, user_ratings in top_n.items():
-    user_ratings.sort(key=lambda x: x[1], reverse=True)
-    #top_n[uid] = user_ratings[:n]
-    top_n[uid] = [rate for rate in user_ratings if rate[1] >= threshold]
-  return top_n
-top_n = get_top_n(predictions, n=10, threshold = 4.1)
-# Print the recommended items for each user
-for uid, user_ratings in top_n.items():
-  print(uid, [(iid, rate ) for (iid, rate) in user_ratings])
-
-
-# In[ ]:
-
-
-
+for trainset, testset in pkf.split(data):
+    algo = SVD()
+    #algo = KNNBaseline(bsl_options=bsl_options, sim_options=sim_options)
+    algo.fit(trainset)
+    ### test
+    predictions = algo.test(testset)
+    ### accuracy:   rmse(root mean square error)     mae(mean absolute error)      fcp
+    # Compute and print Root Mean Squared Error
+    accuracy.rmse(predictions, verbose=True)
+    top_n = get_top_n(predictions, n=10)
+    # Print the recommended items for each user
+    #for uid, user_ratings in top_n.items():
+    #    print(uid, [(iid, rate ) for (iid, rate) in user_ratings])
+    precisions, overall = calculate_percision(top_n, testset)
+    print overall
+    #print precisions
